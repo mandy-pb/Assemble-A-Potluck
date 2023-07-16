c74243812c1c1c4166965a8f3fccb246108e6cae
Mandy PB <plaisance.mandy@gmail.com>
2023-07-16 18:18:20 -0400

    Added potluck items, assigned items to each guest, made assign button work all in javascript.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ const addGuestButton = document.querySelector(".invite");
 const guestInputLabel = document.querySelector(".add-guest label");
 // text input box
 const guestInput = document.querySelector(".add-guest input");
-// unordered list (not yet visible)
+// unordered list 
 const guestList = document.querySelector(".guest-list");
 // span class for number of guests attending
 const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
+// alert when guest list is full 
 const guestFull = document.querySelector(".alert");
+// assign button
+const assignButton = document.querySelector(".assign");
+// list assigned items
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
   const guest = guestInput.value;
@@ -40,3 +44,27 @@ const updateGuestCount = function () {
     guestFull.classList.remove("hide");
   }
 };
+
+const assignItems = function () {
+  const potluckItems = ["Potato Salad", "Hummus", "Cookies", "Fruit", "Hand Sandwiches", "Pig in a Blanket",
+  "Pasta Salad", "Banana Pudding", "Strawberry Cake", "Icecream", "Soda", "Seltzers"];
+
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
